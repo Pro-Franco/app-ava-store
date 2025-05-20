8381ab0135f8c0cf64d01aa109ec6f7f6521728a
franco <r.franco.s.facily@gmail.com>
2025-05-20 15:24:24 -0300

    more dev

--- src/app/libs/axios.ts
+++ src/app/libs/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
+const token = '1234';
+
 export const api = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com'
+  baseURL: 'https://jsonplaceholder.typicode.com',
+  headers: {
+    Authorization: token
+  }
 });
 
 api.interceptors.request.use(
@@ -16,6 +21,14 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+/*api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token'); // ou AsyncStorage no React Native
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});*/
 
 //O interceptador so pode ser usado com uma instancia do axios
 //antes de ser enviado para o servidor - request
